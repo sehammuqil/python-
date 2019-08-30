9d68788c0cf4df25f511ae3ca11818e0bb7deb82
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-08-30 08:35:41 +0300

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,29 @@
+a="hello , world! "
+print(a.strip())
+
+
+
+a="hello, world!"
+print(len(a))
+
+
+a="HELLO, WORLD!"
+print(a.lower())
+
+
+
+a="hello , world !"
+print(a.upper())
+
+
+
+a= "hello , world !"
+print(a.replace('h','j'))
+
+
+
+
+a="hello , world !"
+print(a.split(","))
+
+
