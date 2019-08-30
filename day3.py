0f7fba2c8b6cb7d097844f62a16e81aa20dcb979
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-08-30 05:03:05 +0300

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,48 @@
+x= 5
+y= "john"
+print(x)
+print(y)
+
+
+x= 4  # x is of type int
+x = "sally"  # x is now of type str
+print(x)
+x= "john"
+print(x)
+x= 'john'
+print (x)
+
+
+x , y , z = "orange" , "banana" , "cherry"
+print(x)
+print(y)
+print(z)
+
+
+x=y=z = "orange"
+print(x)
+print(y)
+print(z)
+
+
+
+x = "awesome"
+print("python is " + x )
+
+x = "python is "
+y = "awesome"
+z = x + y
+print (z)
+
+
+
+x=5
+y=10
+print (x + y)
+
+
+
+
+
+
+
