db1d74c036e912d3afaec0a1efa8886ad895763f
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-08-30 05:34:42 +0300

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,61 @@
+x=1 #int
+y=2.8 #float
+z=1j #complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x= 1
+y= 35622255887711
+z= -325522
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x=1.10
+y=1.0
+z= -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+x= 35e3
+y=12E4
+z= -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+x= 3+5j
+y= 5j
+z= -5j
+print(type(x))
+print(type(y))
+print(type(y))
+
+
+
+x= 1 #int
+y = 2.8 #float
+z=1j #complex
+#convert
+a = float (x)
+b = int (y)
+c = complex (x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+
+import random
+print(random.randrange(1,10))
