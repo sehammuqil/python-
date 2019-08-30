820d99a4e5f110aae42583ef8a62c7b9baf499d6
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-08-30 08:02:50 +0300

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,26 @@
+print("hello")
+print('hello')
+
+
+
+a="hello"
+print (a)
+
+
+a = """lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incdiddunt
+ ut labore et dolore magna aliqua"""
+print(a)
+
+
+
+a = "hello , world !"
+print(a[1])
+
+
+b = "hello , world !"
+print(b[2:5])
+
+
+
