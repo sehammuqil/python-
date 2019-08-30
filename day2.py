65b87a1ae9920025a70f6a6724e4e3c48e614e7c
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-08-30 04:33:33 +0300

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,24 @@
+#this is a comment.
+print ("hello , world!")
+print("hello , world!")  #this is a comment.
+
+
+#print ("hello , world!")
+print("cheers , mate!")
+
+#this is a comment
+#written in
+#more then just one line
+print ("hello , world!")
+
+""" this is a comment 
+written in
+ more than just one line 
+"""
+print ("hello , world!")
+
+
+
+
+
+
