724773926bfaf3e238ab7ee80fee2d97173a954a
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-03 01:24:35 +0300

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,32 @@
+thislist=["A","B","C","D","E"]
+print(thislist[1:3])
+print(thislist[0:3])
+
+
+
+thislist=['apple','banana','cherry']
+if 'apple' in thislist:
+    print("yes, 'apple'is in the fruits list")
+
+
+
+thislist=['python']*4
+print(thislist)
+
+
+thislist1=["ahmad","khalid","omar"]
+thislist2=["sara","hind","batool"]
+thislist3=thislist1 + thislist2
+print(thislist3)
+
+
+
+num1=[1,2,3]
+num2=[1.5,2.5,3.5]
+num3=num1+num2
+print(num3)
+
+
+
+
+
