c43c15010f5c6f1bdb075c71b1113daac0229d38
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-03 01:09:56 +0300

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,78 @@
+s = []
+print(s)
+
+
+
+num = [1,2,3,4,5]
+print(num)
+
+
+
+thislist = ['apple','banana','cherry']
+print(thislist)
+
+
+
+thislist=['apple','banana','cherry',1,2,3]
+print(thislist)
+
+
+
+
+thislist = ['apple','banana','cherry']
+print(thislist[1])
+
+
+
+
+thislist = ['apple','banana','cherry']
+for x in thislist:
+   print(x)
+
+
+
+numberslist=[1,2,3,4,5]
+for y in numberslist:
+    print(y)
+
+
+thislist = ['apple','banana','cherry']
+thislist[1]='blackcurrant'
+print(thislist)
+
+
+
+
+
+thislist = ['apple','banana','cherry']
+del thislist[0]
+print(thislist)
+
+
+
+
+thislist = ['apple','banana','cherry']
+del thislist
+
+
+
+thislist=["A","B","C","D","E"]
+print(thislist)
+
+
+thislist=["A","B","C","D","E"]
+del thislist[0]
+print(thislist)
+
+
+
+
+
+
+
+
+
+
+
+
+
