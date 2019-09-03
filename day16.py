b48f70ed28819b8b802049778b49dd3959d5ae86
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-03 13:07:32 +0300

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,51 @@
+thistuple = ("apple","banana","cherry")
+print(thistuple)
+
+thistuple=()
+print(thistuple)
+
+
+thistuple=(3,)
+print(thistuple)
+
+
+
+thistuple=(3,1.3,4.1,7)
+print(thistuple)
+
+
+
+thistuple=("ahmad",1.1,4,"python")
+print(thistuple)
+
+
+
+thistuple = ("apple","banana","cherry")
+print(thistuple[1])
+
+
+thistuple = ("apple","banana","cherry")
+for x in thistuple:
+  print(x)
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple)
+
+
+
+thistuple = ("apple", "banana", "cherry","orange")
+print(thistuple [0:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
