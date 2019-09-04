a970dfec4c7f843f84f6fc1bd843894b9e0658d7
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-04 09:39:21 +0300

    Add files via upload

---
+++ day18,19.py
@@ -0,0 +1,31 @@
+#list
+student =['sara','nora','hind']
+print(student)
+
+
+#append
+student =['sara','nora','hind']
+student.append("seham")
+print(student)
+
+
+#insert
+student =['sara','nora','hind']
+student.insert(2,"seham")
+print(student)
+
+#pop
+student =['sara','nora','hind']
+student.pop(2)
+print(student)
+
+
+#clear
+student =['sara','nora','hind']
+student.clear()
+print(student)
+
+
+tuple = ("java","python","swift")
+if "python" in tuple:
+    print("yes python in the tuple")
