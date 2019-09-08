c0d3d33d19ff7ed62f1580ae0abb56839fe7da20
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-08 08:16:22 +0300

    Add files via upload

---
+++ day20.py
@@ -0,0 +1,32 @@
+thisset = {}
+print(thisset)
+
+
+
+thisset = {'apple','banana','cherry'}
+print(thisset)
+
+
+thisset = {'ahmad','ahmad',1,2,1,5}
+print(thisset)
+
+
+thisset = {'apple','banana','cherry'}
+for x in thisset:
+  print(x)
+
+thisset = {'apple', 'banana', 'cherry'}
+print('banana' in thisset)
+
+
+
+thisset = {'apple','banana','cherry'}
+thisset.add('orange')
+print(thisset)
+
+
+
+thisset = {'apple','banana','cherry'}
+thisset.update(['orange','mango','grapes'])
+print(thisset)
+
