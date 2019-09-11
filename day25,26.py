b73199de5133aa4991d2c6e12501ea4622a055c5
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-12 01:20:49 +0300

    Add files via upload

---
+++ day25,26.py
@@ -0,0 +1,21 @@
+#1
+set={1,3,5,7,8}
+print(set)
+set.remove(8)
+print(set)
+set.clear()
+print(set)
+
+#2
+dict={
+    "name":"pigeon",
+    "type":"bird",
+    "skincover":"feathers"
+}
+print(dict)
+
+x=dict["type"]
+print(x)
+
+dict["skincover"]= "nonfeathers"
+print(dict)
