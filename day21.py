2696a1b365ff586c39b02547b65722bc28267dc6
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-11 13:27:00 +0300

    Add files via upload

---
+++ day21.py
@@ -0,0 +1,46 @@
+thisset = {'apple','banana','cherry'}
+print (len(thisset))
+
+
+
+thisset = {'apple','banana','cherry'}
+thisset.remove('banana')
+print (thisset)
+
+
+
+thisset = {'apple','banana','cherry'}
+thisset.discard('banana')
+print (thisset)
+
+
+
+thisset = {'apple','banana','cherry'}
+x = thisset.pop()
+print(x)
+print (thisset)
+
+
+
+
+thisset = {'apple','banana','cherry'}
+thisset.clear()
+print (thisset)
+
+
+'''#run is error
+thisset = {'apple','banana','cherry'}
+del thisset
+print (thisset)'''
+
+
+
+
+thisset = set(('apple','banana','cherry'))
+print (thisset)
+
+
+
+
+
+
