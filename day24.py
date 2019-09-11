a4cf10926c0110083e2575ef2dc175a996f27277
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-12 00:47:25 +0300

    Add files via upload

---
+++ day24.py
@@ -0,0 +1,66 @@
+thisdict = {
+    "brabd":"ford",
+    "model":"mustang",
+    "year":1964
+}
+mydict= thisdict.copy()
+print(mydict)
+mydict=dict(thisdict)
+print(mydict)
+
+
+
+
+myfamily = {
+    "child1": {
+        "name":"emil",
+        "year":2004
+    },
+    "child2": {
+        "name":"tobias",
+        "year":2007
+    },
+    "child3":{
+        "name":"linus",
+        "year":2011
+    }
+    }
+
+print(myfamily)
+
+child1: {
+    "name": "emil",
+    "year": 2004
+}
+child2: {
+    "name": "tobias",
+    "year": 2007
+}
+child3: {
+    "name": "linus",
+    "year": 2011
+}
+
+#myfamily = {
+  #  "child1" : child1,
+   # "child2": child2,
+   # "child3":child3}
+
+
+print(myfamily)
+
+
+
+
+thisdict=dict(brand="ford",model="mustang",year=1964)
+print(thisdict)
+
+
+
+
+
+
+
+
+
+
