0af65a7c8db5409aab01e9296bce93409b256520
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-20 13:16:56 +0300

    Add files via upload

---
+++ day30.py
@@ -0,0 +1,39 @@
+for x in range (6):
+    print(x)
+
+
+
+
+for x in range (2,6):
+    print(x)
+
+
+
+
+for x in range (2,30,3):
+    print(x)
+
+
+
+
+for x in range(6):
+    print(x)
+else:
+    print("finally finished!")
+
+
+
+
+
+
+adj = ["red","big","tasty"]
+fruits = ["apple","banana","cherry"]
+
+for x in adj :
+    for y in fruits:
+        print(x,y)
+
+
+
+
+
