156f0cb07f8cdbf44bd0eb04283086b04f328d25
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-09-20 13:33:19 +0300

    Add files via upload

---
+++ day31.py
@@ -0,0 +1,31 @@
+def my_function():
+    print ("hello from a function")
+
+my_function()
+
+
+
+
+def my_function(fname):
+    print(fname +  "refsnes")
+
+my_function("emil")
+my_function("tobias")
+my_function("linus")
+
+
+
+
+def my_function(country = "norway"):
+    print("i am from "+ contry)
+
+    my_function("sweden")
+    my_function("india")
+    my_function()
+    my_function("brazil")
+
+
+
+
+    
+
