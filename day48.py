3265b26a6d37fba5b2a3a17bb5a50ed48ff8b137
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-10-17 14:52:51 +0300

    Add files via upload

---
+++ day48.py
@@ -0,0 +1,31 @@
+def myfunc():
+  x=300
+  print(x)
+
+myfunc()
+
+#_____________
+
+def myfunc():
+    x=300
+    def myinnerfunc():
+      print(x)
+    myinnerfunc()
+myfunc()
+
+
+#______________
+
+
+x=300
+
+def myfunc():
+   print(x)
+myfunc()
+
+print(x)
+
+
+#_____________
+
+
