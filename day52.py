191115c804f1ab12577e7993052f1de5f562f611
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-10-21 17:36:21 +0300

    Add files via upload

---
+++ day52.py
@@ -0,0 +1,34 @@
+import datetime
+
+x =datetime.datetime.now()
+print(x)
+
+#______________________
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+#_______________________
+
+import datetime
+x=datetime.datetime(2020,5,17)
+print(x)
+
+
+#_________________________
+
+import datetime
+
+x= datetime.datetime(2018,6,1)
+print(x.strftime("%B"))
+
+#_________________________
+
+
+
+
