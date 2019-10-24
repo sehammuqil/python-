6cea40732644c1883622a32fe2e9e692f816ca54
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-10-25 00:15:08 +0300

    Add files via upload

---
+++ day67,68.py
@@ -0,0 +1,13 @@
+print("enter first char in your name")
+x = input()
+print("enter last char in your name ")
+y = input()
+print("start your name:",x )
+print("end your name :",y)
+
+#_______________________
+
+
+masseg = 53.44
+txt="dear ahmad ali , your current balance is {:2}$"
+print(txt.format(masseg))
