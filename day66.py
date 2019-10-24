b8d80eaa5c817e388d23e3511ffa7fc748548b78
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-10-25 00:02:05 +0300

    Add files via upload

---
+++ day66.py
@@ -0,0 +1,20 @@
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity,itemno,price))
+
+#__________________________
+
+age =36
+name = "john"
+txt = "his name is {1}.{1} is {0} years old."
+print(txt.format(age,name))
+
+#_________________________
+
+myorder = "I have a {carname},it is a {model}"
+print(myorder.format(carname="ford", model= "mustang"))
+
+#_______________________
+
