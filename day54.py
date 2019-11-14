2b6c21a3d6b992674dc3445062b786a76ded8d80
sehammuqil <54260973+sehammuqil@users.noreply.github.com>
2019-11-15 01:11:15 +0300

    Add files via upload

---
+++ day54.py
@@ -0,0 +1,16 @@
+
+import calmodule
+
+print("1+8=",calmodule.add(1,8))
+print("4-2=",calmodule.sub(4,2))
+print("6*6=",calmodule.multiply(6,6))
+print("8/2=",calmodule.divide(8,2))
+
+
+#_____________
+import datetime
+
+print(datetime.datetime.now().year )
+print(datetime.datetime.now().strftime("%b"))
+print(datetime.datetime.now())
+print(datetime.datetime.now().day)
